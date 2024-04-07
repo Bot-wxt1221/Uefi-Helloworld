d6b0c0855f54aec9fff5a39d90553c1c37de70ff
wxt <3264117476@qq.com>
2024-04-07 20:52:56 +0800

    Init Protocol

--- Helloworld.c
+++ Helloworld.c
@@ -1,9 +1,24 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
-
+#include <Library/UefiBootServicesTableLib.h>
 EFI_STATUS
 EFIAPI
 UefiMain(EFI_HANDLE IN ImageHandle,EFI_SYSTEM_TABLE IN *Systemtable){
   Print(L"Hello World! Let Start edk2!\n");
+  EFI_STATUS ret=EFI_SUCCESS;
+  UINTN NoHandles=0;
+  EFI_HANDLE *buffer=NULL;
+  ret=gBS->LocateHandleBuffer(ByProtocol,&gEfiGraphicsOutputProtocolGuid,NULL,&NoHandles,&buffer);
+  if(EFI_ERROR(ret)){
+    Print(L"Error while get Handle buffer");
+  }
+  Print(L"ret %d\n",ret);
+  Print(L"NoHandles %d\n",NoHandles);
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
+  ret=gBS->OpenProtocol(buffer[0],&gEfiGraphicsOutputProtocolGuid,(void **)&Gop,ImageHandle,NULL,EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+  Print(L"ret %d\n",ret);
+  if(EFI_ERROR(ret)){
+    Print(L"Error %d",ret);
+  }
   return EFI_SUCCESS;
 }
